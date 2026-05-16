35165600dbf8c0064ee3a394e9a12f85372bbc6c
Swaroop R <rswaroop0608@gmail.com>
2026-05-16 17:36:03 +0530

    Fix: Remove invalid allowedDevOrigins config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ["192.168.29.53"],
-  },
 };
 
 export default nextConfig;
